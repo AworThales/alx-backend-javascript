915b7f4ea5024ea95e56f96069df8068bc355ed8
AworThales <thals4jesus@gmail.com>
2023-11-29 22:47:34 +0000

    fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const res = [];
+  const newArray = [];
+
   for (const value of array) {
-    res.append(appendString + value);
+    newArray.push(appendString + value);
   }
 
-  return res;
+  return newArray;
 }
